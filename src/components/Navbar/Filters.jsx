2717034cf5a657c0350f426b47d1ce9b8ffa1b69
Aalya Vora <40417175+aalya2406@users.noreply.github.com>
2024-07-11 21:05:41 +0530

    Update Filters.jsx
    Filters updates to ensure sorting alphabetically and all the updated works

--- src/components/Navbar/Filters.jsx
+++ src/components/Navbar/Filters.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './Filters.css';
 import { fetchAllAreas } from '../../services/api';
 
-const Filters = ({ setSelectedArea }) => {
+const Filters = ({ setSelectedArea, handleSort, handlePureVeg }) => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [areas, setAreas] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState(null);
 
-  const filters = ['Filter By Area', 'Under Rs.300', 'Desserts', 'Starters'];
+  const filters = ['Filter By Area', 'Under Rs.300', 'Desserts', 'Starters', 'Sort Alphabetically', 'Pure Veg'];
 
   const toggleDropdown = (filter) => {
     if (selectedFilter === filter) {
@@ -42,10 +42,20 @@ const Filters = ({ setSelectedArea }) => {
         <div
           key={filter}
           className={`filter-tag ${selectedFilter === filter ? 'highlighted' : ''}`}
-          onClick={() => toggleDropdown(filter)}
+          onClick={() => {
+            if (filter === 'Sort Alphabetically') {
+              handleSort(); // Call the handleSort function passed as prop
+              setSelectedFilter(filter); // Highlight the 'Sort Alphabetically' filter
+            } else if (filter === 'Pure Veg') {
+              handlePureVeg(); // Call the handlePureVeg function passed as prop
+              setSelectedFilter(filter); // Highlight the 'Pure Veg' filter
+            } else {
+              toggleDropdown(filter);
+            }
+          }}
         >
           <span>{filter}</span>
-          <span className={`arrow ${selectedFilter === filter && showDropdown ? 'up' : 'down'}`}>&#9660;</span>
+          {filter === 'Filter By Area' && <span className={`arrow ${selectedFilter === filter && showDropdown ? 'up' : 'down'}`}>&#9660;</span>}
         </div>
       ))}
       {showDropdown && selectedFilter === 'Filter By Area' && (
