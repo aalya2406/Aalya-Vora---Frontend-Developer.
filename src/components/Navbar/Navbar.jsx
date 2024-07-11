99e01ecba0530a54b44e62107b89570c7baa273f
Aalya Vora <40417175+aalya2406@users.noreply.github.com>
2024-07-11 21:10:05 +0530

    Update Navbar.jsx
    updating the dimensions and working of search bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,22 @@
-// Navbar.jsx
 
 import React from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png'; // Adjust the path based on your directory structure
 import search_icon from '../../assets/search_icon.png'; // Ensure this path is correct
 
+
 const Navbar = () => {
   const handleLogoClick = () => {
-    window.location.reload(); // Reloads the page
+    window.location.reload();
   };
 
   return (
     <div className='Navbar'>
-      <a href="/" onClick={handleLogoClick} className="logo-link">
-        <img src={logo} alt="Logo" className="logo" />
-      </a>
-      <ul className="navbar-menu">
-        {/* Menu items can go here */}
-      </ul>
+      <img src={logo} alt="Logo" className="logo" onClick={handleLogoClick} />
+  
       <div className="search-bar">
         <img src={search_icon} alt="Search Icon" className="navbar-search-icon" />
-        <input type="text" placeholder="Search for food..." />
+        <input type="text" placeholder="Search for restaurants and food" />
       </div>
     </div>
   );
